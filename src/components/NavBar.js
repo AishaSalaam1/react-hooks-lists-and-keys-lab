497a30cca12e619ddb52f3958d9e673a6c48a032
Aisha Hussein <aishasalaam07@gmail.com>
2023-04-23 13:04:44 -0400

    completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (<nav>
+    {/* display an <a> tag for each link here */}
+    {links.map((link, index)=>{
+      return(
+        <a key={index} href="jhui">{link}</a>
+      )})}
+    </nav>);
 }
 
 export default NavBar;
